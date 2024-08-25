51822021ba7b300c6cde8f0b9273658316cace6d
son_dovan <dovansonbk@gmail.com>
2024-08-26 00:05:28 +0700

    20240825 1:06:55

---
+++ src/Components/NewsLetter/NewLetter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  './NewLetter.css'
+
+export const NewLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>Get Exclusive Ofeers On Your Email</h1>
+        <p>Subscribe</p>
+    </div>
+  )
+}
