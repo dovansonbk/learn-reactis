9cbc15506f55b83f86633c577875b631f35dc9a5
son_dovan <dovansonbk@gmail.com>
2024-08-26 19:51:12 +0700

    20240826 1:23:14

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ShopCategory } from './Pages/ShopCategory';
 import { LoginSignup } from './Pages/LoginSignup';  // Named import
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
+import { Footer } from './Components/Footer/Footer';
 function App() {
   return (
     <div>
@@ -23,6 +24,7 @@ function App() {
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
